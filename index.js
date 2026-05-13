fbf5e6edea0e04f4918d30bf655f46be58e48d4c
Yoselin Ccapera Herencia <Yoselin.ccapera@tecsup.edu.pe>
2026-05-12 22:50:55 -0500

    fix respuesta http

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 const http = require('http')
 
-function requestController(){
-    console.log('Bienvenidos al curso')
+function requestController(req, res){
+    res.write("Bienvenidos al curso ");
+    res.end();
 }
-
 const server = http.createServer(requestController)
 
 const PORT = process.env.PORT
